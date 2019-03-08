92551cfca18c1f0b21f0b6f71581f3db8aa59c6b
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-08 22:03:53 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+for (let i=0; i < 25; i++){
+  if (i==="1")
+  {
+    console.log("I am 1 strange loop.");
+  }
+  else (i<25)
+  {
+    console.log("I am ${i} strange loops.")
+  }
+  
+  
+  
+  
+}
